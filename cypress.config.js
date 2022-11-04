d09e6b08d01f3a10bcc22f8fa0d9b40b03b9fa2b
Shibin Madassery <shibin.m@gmail.com>
2022-11-05 01:11:06 +0400

    changed projectid
    updated project id

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'q6vm4y',
+  projectId: 'yad7on',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
