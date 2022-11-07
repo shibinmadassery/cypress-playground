0e40365d31dc7a6239b30babae917e2779a911ff
Shibin Madassery <shibin.m@gmail.com>
2022-11-07 14:48:41 +0400

    added test
     - login to gumroad

---
+++ cypress/e2e/loginGumroad.cy.js
@@ -0,0 +1,6 @@
+import { gumroadLogin, TestFunction01 } from "./utlis";
+describe("Login to Gumroad", () => {
+  it("login to gumroad and verify after login", () => {
+    gumroadLogin();  
+  });
+});
